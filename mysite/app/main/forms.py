1f0412aa6af434ca865d1b4cdb986f788fd5c837
venthent <13533801264@163.com>
2018-07-28 22:01:28 +0800

    Can edit user information and change password

--- mysite/app/main/forms.py
+++ mysite/app/main/forms.py
@@ -1,30 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, RadioField, StringField, PasswordField, SelectField
+from wtforms import SubmitField, StringField, PasswordField, SelectField
 from wtforms.validators import DataRequired
 
 
 class AccountForm(FlaskForm):
-    '''For Add account'''
-    name = StringField("Account name:", validators=[DataRequired()], render_kw={'placeholder': 'Username,for login'})
-    real_name = StringField("Real name:", validators=[DataRequired()])
-    position = SelectField(label="Job position:",validators=[DataRequired()],
-                           choices=[('Staff', 'Staff'), ('Manager', 'Manager'), ('Boss', 'Boss')])
-
-    submit = SubmitField('Confirm')
-
-class EditForm1(FlaskForm):
-    '''For Add account while current_user is user and current_user's permission is Administrator'''
     name = StringField("Account name:", validators=[DataRequired()], render_kw={'placeholder': 'Username,for login'})
     real_name = StringField("Real name:", validators=[DataRequired()])
     position = SelectField(label="Job position:", validators=[DataRequired()],
                            choices=[('Staff', 'Staff'), ('Manager', 'Manager'), ('Boss', 'Boss')])
     permission = SelectField(label="Permission:", validators=[DataRequired()],
                              choices=[('Administrator', 'Administrator'), ('User', 'User')])
-
     old_password = PasswordField("Old password:", render_kw={
         'placeholder': 'If you write nothing,origional password will not be changed.'})
     new_password = PasswordField("New password:")
     confirm_password = PasswordField("Confirm new password:",
                                      render_kw={'placeholder': 'Please confirm your new password again'})
     submit = SubmitField('Confirm')
-
--- mysite/app/main/views.py
+++ mysite/app/main/views.py
@@ -1,6 +1,6 @@
 from flask import url_for, render_template, flash, redirect, request
 from flask_login import logout_user, login_user, login_required, current_user
-from ..models import User, Process
+from ..models import User, Process, Role
 from mysite.app.main import main
 from .forms import AccountForm
 from .. import db
@@ -18,6 +18,7 @@ def index():
 
 
 @main.route('/sys/account/')  # 管理帐号
+@login_required
 def account_manage():
     per = current_user.role.permission
     if per == 'Administrator':
@@ -29,6 +30,7 @@ def account_manage():
 
 
 @main.route('/sys/add', methods=['POST', 'GET'])
+@login_required
 def add_account():
     form = AccountForm()
     u = User.query.filter_by(name=form.name.data).first()
@@ -48,37 +50,35 @@ def add_account():
 
 
 @main.route('/sys/edit/<int:id>', methods=['POST', 'GET'])
+@login_required
 def edit_account(id):
+    user = User.query.get_or_404(id)
     form = AccountForm()
-    user = User.query.filter_by(id=id).first()
     if form.validate_on_submit():
-        if current_user == user and current_user.role.permission == 'Administrator':
-            user.name = form.name.data
+        if current_user == user:
             user.real_name = form.real_name.data
-            user.position = form.position.data
-            user.role.permission = form.permission.data
-            if form.old_password is not None and form.new_password is not None and form.confirm_password is not None:
-                if user.verity_password(form.old_password) and form.new_password == form.confirm_password:
-                    user.password=form.new_password
-            db.session.add(user)
-            db.session.commit()
-            return redirect(url_for('main.account_manage'))
-        elif current_user.role.permission=='Administrator':
-            user.name = form.name.data
-            user.real_name = form.real_name.data
-            user.position = form.position.data
-            user.role.permission = form.permission.data
+            if form.old_password.data.strip() != '' and form.new_password.data.strip() != '' and form.confirm_password.data.strip() != '':
+                if user.verity_password(form.old_password.data) and form.new_password.data ==form.confirm_password.data:
+                    user.password = form.new_password.data
+            if current_user.role.permission == 'Administrator':
+                user.name = form.name.data
+                user.position = form.position.data
+                r = Role(permission=form.permission.data)
+                user.role = r
             db.session.add(user)
             db.session.commit()
             return redirect(url_for('main.account_manage'))
         else:
-            user.real_name = form.real_name.data
-            if form.old_password is not None and form.new_password is not None and form.confirm_password is not None:
-                if user.verity_password(form.old_password) and form.new_password == form.confirm_password:
-                    user.password=form.new_password
-            db.session.add(user)
-            db.session.commit()
-            return redirect(url_for('main.account_manage'))
+            if current_user.role.permission == 'Administrator':
+                user.real_name = form.real_name.data
+                user.name = form.name.data
+                user.real_name = form.real_name.data
+                user.position = form.position.data
+                r = Role(permission=form.permission.data)
+                user.role = r
+                db.session.add(user)
+                db.session.commit()
+                return redirect(url_for('main.account_manage'))
     form.name.data = user.name
     form.real_name.data = user.real_name
     form.position.data = user.position  # Admin can see only
